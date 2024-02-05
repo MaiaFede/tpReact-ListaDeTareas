5226c5182fdeeecb945829a448df347522201714
Maia Federica_G <maiafedericag@gmail.com>
2024-02-05 14:29:10 -0300

    Finalice el proyecto solo me falta el readme

--- src/components/ListaTareas.jsx
+++ src/components/ListaTareas.jsx
@@ -30,15 +30,28 @@ const tareaGuardada = (event)=> {
         setTareas(tareasNuevas);
       }
     return (
-      
+
         <div className="container-fluid align-center text-center ">
-            <h1>Bienvenido</h1>
-            <h4>Ingresa tus tareas</h4>
-            <input type="text" value={nuevaTarea} onChange={tareaGuardada} />  
-            <button onClick={añadirTarea} className="btn btn-primary">Agregar tarea</button>
-            <ul className="list-group"></ul>
-      </div>
-    );
+                    <h1>Bienvenido</h1>
+                    <h4>Ingresa tus tareas</h4>
+                    <input type="text" value={nuevaTarea} onChange={tareaGuardada} />  
+                    <button onClick={añadirTarea} className="btn btn-primary rounded-4 ms-2">Agregar tarea</button>
+                    <ul className="list-group">
+        {tareas.map((tarea, index) => (
+                  <li key={index} className="list-unstyled mt-3 row ">
+                    <div className="conteiner-fluid border rounded-pill d-flex justify-content-start col-7 mx-4">
+                    <input type="checkbox" checked={tarea.completed} onChange={() => completarTarea(index)} className="ms-5" />
+                    <p className='mt-3 me-5 ms-2'> {tarea.text} </p>
+                    
+                    </div>
+                    <button onClick={() => eliminarTarea(index)} className="btn btn-danger col-4">Eliminar</button>
+                  </li>
+                ))}
+
+                    
+                    </ul>    
+                </div>
+            );
 };
 
 export default ListaTareas;
