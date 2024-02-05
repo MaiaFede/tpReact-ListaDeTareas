55e4ea326f15681bc3400d4aa35bf8cdd58e74cf
Maia Federica_G <maiafedericag@gmail.com>
2024-02-05 13:59:09 -0300

    Hice la funcion eliminar y cambie variables a español

---
+++ src/components/ListaTareas.jsx
@@ -0,0 +1,39 @@
+import {useState} from 'react';
+
+const ListaTareas = () => {
+    const [tareas, setTareas] = useState([]);
+    const [nuevaTarea, setNuevaTarea] = useState("");
+const tareaGuardada = (event)=> {
+    
+        setNuevaTarea(event.target.value);
+    }
+      function añadirTarea() {
+        const tareaNueva = {
+          text: nuevaTarea,
+          completed: false,
+        };
+    
+        setTareas([...tareas, tareaNueva]);
+        setNuevaTarea("")
+      }
+      function eliminarTarea(index) {
+        const tareasNuevas = tareas.slice();
+        tareasNuevas.splice(index, 1);
+    
+        setTareas(tareasNuevas);
+
+      }
+      
+    return (
+      
+        <div className="container-fluid align-center text-center ">
+            <h1>Bienvenido</h1>
+            <h4>Ingresa tus tareas</h4>
+            <input type="text" value={nuevaTarea} onChange={tareaGuardada} />  
+            <button onClick={añadirTarea} className="btn btn-primary">Agregar tarea</button>
+            <ul className="list-group"></ul>
+      </div>
+    );
+};
+
+export default ListaTareas;
